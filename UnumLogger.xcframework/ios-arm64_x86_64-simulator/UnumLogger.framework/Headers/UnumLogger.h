ac5c6e29f0b0a7b611bdba2cb86a6af0091b6b31
lukaszdw <lucas@unumid.co>
2022-09-15 17:58:06 +0200

    Initial commit

---
+++ UnumLogger.xcframework/ios-arm64_x86_64-simulator/UnumLogger.framework/Headers/UnumLogger.h
@@ -0,0 +1,18 @@
+//
+//  UnumLogger.h
+//  UnumLogger
+//
+//  Created by Lucas Dworakowski on 01/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnumLogger.
+FOUNDATION_EXPORT double UnumLoggerVersionNumber;
+
+//! Project version string for UnumLogger.
+FOUNDATION_EXPORT const unsigned char UnumLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnumLogger/PublicHeader.h>
+
+
